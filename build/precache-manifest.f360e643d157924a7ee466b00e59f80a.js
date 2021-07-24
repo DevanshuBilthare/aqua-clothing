d3bea5261981812b3cb802cfedf3c271323d04bd
Devanshu Bilthare <devanshubilthare821@gmail.com>
2021-07-24 20:55:20 +0530

    b

---
+++ build/precache-manifest.f360e643d157924a7ee466b00e59f80a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd6de00acd86193583f8c9dc6082101",
+    "url": "/aqua-clothing/index.html"
+  },
+  {
+    "revision": "288adad3bb03e6aa2e41",
+    "url": "/aqua-clothing/static/css/main.0f5bc524.chunk.css"
+  },
+  {
+    "revision": "52448af34b76d1281f95",
+    "url": "/aqua-clothing/static/js/2.c1442c55.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/aqua-clothing/static/js/2.c1442c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288adad3bb03e6aa2e41",
+    "url": "/aqua-clothing/static/js/main.17323577.chunk.js"
+  },
+  {
+    "revision": "15797c92bad64819de33",
+    "url": "/aqua-clothing/static/js/runtime-main.f7f74809.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/aqua-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/aqua-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
